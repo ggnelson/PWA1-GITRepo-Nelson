f52757721132c5b9263e4fe7f0cf32aad4f569b8
ggnelson <gilbertnelson@Gilberts-MBP.home>
2015-10-09 07:12:39 -0400

    fixed week one

--- WK1/goal1_assign_duel1_Nelson_Gilbert/js/main.js
+++ WK1/goal1_assign_duel1_Nelson_Gilbert/js/main.js
@@ -27,9 +27,17 @@
 
     console.log("FIGHT!!!");
 
-    //player array
-    var playerOne = ["Spiderman", 20, 100];
-    var playerTwo = ["Batman", 20, 100];
+    //player name
+    var playerOneName = "Spiderman";
+    var playerTwoName = "Batman";
+
+    //player damage
+    var player1Damage = 20;
+    var player2Damage = 20;
+
+    //player health
+    var playerOneHealth = 100;
+    var playerTwoHealth = 100;
 
     //initiate round
     var round=0;
